d477435fad5fe53c19622d1b2b64e085f00516a0
bhagya1330-pb <21up1a05g8.vmtw@gmail.com>
2024-06-21 13:26:08 +0530

    Add files via upload

---
+++ Sqroot.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Sqroot
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+int a;
+a=s.nextInt();
+float b=(float)Math.sqrt(a);
+System.out.println(b);
+}
+}

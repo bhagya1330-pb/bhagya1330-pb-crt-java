45cbb05e8f5f7b0c4b69c3993327a0339f3e879f
bhagya1330-pb <21up1a05g8.vmtw@gmail.com>
2024-06-21 13:58:01 +0530

    Create Perimeter and area.java

---
+++ Perimeter and area.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+    Scanner s=new Scanner(System.in);
+        int l,b;
+        l=s.nextInt();
+        b=s.nextInt();
+        int p1=2*(l+b);
+        int a=l*b;
+        System.out.println(p1);
+        System.out.println(a);
+    }
+}
+Input (stdin)
+
+50
+20
+Your Output (stdout)
+
+140
+1000
+Expected Output
+
+140
+1000

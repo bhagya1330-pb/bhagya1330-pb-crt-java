5779efe5d2b26945c68ba22337fb9135ee1deaff
bhagya1330-pb <21up1a05g8.vmtw@gmail.com>
2024-06-21 13:22:47 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,13 @@
+class Addition
+{
+public static void main(String args[])
+{
+	int a,b,c;
+	a=10;
+	b=12;
+	c=a+b;
+	System.out.println(c);
+	System.out.println("sum="+c);
+	System.out.println("Value of a="+a+" Value of b="+b+" Sum="+c);
+}
+}

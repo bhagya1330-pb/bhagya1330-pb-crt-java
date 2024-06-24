7b88587439f01f53c4816e3cc9766fd84945b4e7
bhagya1330-pb <21up1a05g8.vmtw@gmail.com>
2024-06-24 11:50:23 +0530

    Add files via upload

---
+++ DynamicIP.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class DynamicIP
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+int n=s.nextInt();
+int a[]=new int[5];
+for(int i=0;i<n;i++)
+{
+a[i]=s.nextInt();
+}
+for(int i=0;i<n;i++)
+{
+System.out.println(a[i]+" ");
+}
+}
+}

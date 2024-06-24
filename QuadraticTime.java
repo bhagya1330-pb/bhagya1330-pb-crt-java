873ea8a48024a93a0f4ab5c93a008cc35152e55c
bhagya1330-pb <21up1a05g8.vmtw@gmail.com>
2024-06-24 11:54:00 +0530

    Add files via upload

---
+++ QuadraticTime.java
@@ -0,0 +1,20 @@
+class QuadraticTime
+{
+public static int fun(int n)
+{
+ int c=0;
+	for(int i=0;i<n;i++)
+	{
+	for(int j=0;j<n;j++)
+	{
+	c++;
+	}
+	}
+return c;
+}
+public static void main(String args[])
+{
+int n=100;
+System.out.println("N=100, no.of instructions O(n^2):"+fun(n));
+}
+}
